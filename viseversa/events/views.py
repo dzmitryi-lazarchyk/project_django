343c24cbf42e553677b1b1396a5aadff6b813f57
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2022-09-13 06:41:24 +0300

    Edit uploaded images

--- viseversa/events/views.py
+++ viseversa/events/views.py
@@ -260,7 +260,7 @@ def search_venues(request):
 @authenticated
 def update_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
-    form = VenueForm(request.POST or None, instance=venue)
+    form = VenueForm(request.POST or None, request.FILES or None, instance=venue)
     if form.is_valid():
         form.save()
         return redirect('list-venues')
